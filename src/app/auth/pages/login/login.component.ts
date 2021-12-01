f7efb232a14622a6225cfd044d40f6e7ef4c49e2
Erick Estrada <erickestradapj@gmail.com>
2021-12-01 14:43:58 -0500

    user login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +14,22 @@ export class LoginComponent implements OnInit {
     password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {}
 
-  /**
-   * login
-   */
   public login() {
     console.log(this.myForm.value);
-    console.log(this.myForm.valid);
+    const { email, password } = this.myForm.value;
 
-    this.router.navigateByUrl('/dashboard');
+    this.authService.login(email, password).subscribe((resp) => {
+      console.log(resp);
+    });
+
+    // this.router.navigateByUrl('/dashboard');
   }
 }
